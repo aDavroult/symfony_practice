21c91d120d074f5ac83b12fc604a8911e1e2fff0
aDavroult <pro.armand.davroult@gmail.com>
2019-10-23 15:28:37 +0200

    --- Auto-Git Commit ---

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var Encore = require('@symfony/webpack-encore');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -69,6 +70,11 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
+
+    // images in css
+    .addPlugin(new CopyWebpackPlugin([
+        { from: './assets/img', to: 'img' }
+    ]))
 ;
 
 module.exports = Encore.getWebpackConfig();
